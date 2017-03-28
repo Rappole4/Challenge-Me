8b9c4fae3c8d8f10096440091c0cd72640cbf6f2
Robert <rappole4@yahoo.com>
2017-03-28 11:06:57 -0400

    Almost functional

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -1,5 +1,6 @@
 class CompetitionsController < ApplicationController
   before_action :set_competition, only: [:show, :edit, :update, :destroy]
+  before_action :owned_competition, only: [:edit, :destroy, :update]
 
   # GET /competitions
   # GET /competitions.json
@@ -92,5 +93,12 @@ class CompetitionsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def competition_params
       params.required(:competition).permit(:challenge_id, :game, :name, :goal, :competitor, :participants, :gamecount, :sunshine)
+    end
+    # this plus before action at top, stops alternative users from editing rules of comp
+    def owned_competition
+      unless current_user == @competition.user
+        flash[:alert] = "Sorry, you didn't create this challenge, so you can't edit/destroy it"
+        redirect_to(:back)
+      end
     end
 end
